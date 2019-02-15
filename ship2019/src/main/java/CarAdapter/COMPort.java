ed3ea17495d0a91e9495b1809450397ad3edbc17
Aleksander <aleksander.marciniak@capgemini.com>
2019-02-15 17:55:28 +0100

    COMPort v2 beta

--- ship2019/src/main/java/CarAdapter/COMPort.java
+++ ship2019/src/main/java/CarAdapter/COMPort.java
@@ -2,21 +2,32 @@ package CarAdapter;
 
 import jssc.SerialPort;
 import jssc.SerialPortException;
-import jssc.SerialPortList;
 
 public class COMPort {
 
 	private static final String COM15 = "COM15";
 
+	private static SerialPort SERIAL_PORT = new SerialPort(COM15);
+
 	public static void write(String text) {
-		SerialPort serialPort = new SerialPort(COM15);
+		if (!SERIAL_PORT.isOpened()) {
+			openPort();
+		}
+
 		try {
-			serialPort.openPort();
-			serialPort.setParams(9600, 8, 1, 0);
-			serialPort.writeString(text);
-			serialPort.closePort();
-		} catch (SerialPortException ex) {
-			System.out.println(ex);
+			SERIAL_PORT.writeString(text);
+		} catch (SerialPortException e) {
+			e.printStackTrace();
 		}
 	}
+
+	private static void openPort() {
+		try {
+			SERIAL_PORT.openPort();
+			SERIAL_PORT.setParams(9600, 8, 1, 0);
+		} catch (SerialPortException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
